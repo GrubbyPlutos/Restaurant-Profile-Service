7ad77563055596ff74c9ab8e8a68c1fc4e278a24
Jose Velez <josevelezq7@gmail.com>
2019-01-14 22:33:36 -0800

    Implement CRUD routes in server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('../database');
+const db = require('../database/cassandra/index');
+// const db = require('../database/postgresql/index');
 const app = express();
 const PORT = 3001;
+const morgan = require('morgan');
 const path = require('path');
+const { parseNums } = require('./helpers');
 
+app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -15,9 +19,41 @@ app.get('/restaurants/:id', (req, res) => {
 });
 
 app.get('/restaurants/:id/profile', (req, res) => {
-  db.grabRestaurantInfo(req.params.id, (restaurant) => {
-    res.json(restaurant);
-  });
+  db.getFromDb({ id: Number(req.params.id) })
+    .then(restaurantResult => res.json(restaurantResult))
+    .catch(() => res.status(500));
+});
+
+app.post('/restaurants/', (req, res) => {
+  parseNums(req.body);
+
+  db.postToDb(req.body)
+    .then(restaurantResult => res.json(restaurantResult));
+});
+
+app.put('/restaurants/:id', (req, res) => {
+  parseNums(req.body);
+
+  let updates = {
+    selectors: {
+      id: Number(req.params.id),
+    },
+    updateChanges: req.body,
+  };
+
+  db.updateInDb(updates)
+    .then(restaurantResult => res.json(restaurantResult));
+});
+
+app.delete('/restaurants/:id', (req, res) => {
+  parseNums(req.params);
+
+  let deleteRest = {
+    selectors: req.params,
+  };
+
+  db.deleteFromDb(deleteRest)
+    .then(restaurantResult => res.json(restaurantResult));
 });
 
 app.listen(PORT, () => {
