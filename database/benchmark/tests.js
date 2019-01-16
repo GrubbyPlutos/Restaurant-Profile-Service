91c899207117b81fa9c03653b8af30f25ed615d9
Jose Velez <josevelezq7@gmail.com>
2019-01-15 19:52:42 -0800

    Add update and delete tests by making axios put/delete requests

--- database/benchmark/tests.js
+++ database/benchmark/tests.js
@@ -10,7 +10,7 @@ const getReadAverage = async (dbName, port) => {
     
   for (let i = 0; i < iterations; i += 1) {
     let now = Date.now();
-    let promise = await axios.get(`http://localhost:${port}/restaurants/${get1To10M()}/profile`)
+    let promise = await axios.get(`http://localhost:${port}/restaurants/${get1To10M()}/profile`);
     let later = Date.now();
     sum += later - now;
   }
@@ -29,14 +29,14 @@ const getPostAverage = async (dbName, port) => {
   for (let i = 0; i < iterations; i += 1) {
     let now = Date.now();
     await axios.post(`http://localhost:${port}/restaurants/`, {
-      accuracy: 85,
-      address: 'TEST ADDRESS',
-      delivery: 92,
-      name: 'TEST NAME',
-      number: 'TEST NUMBER 555-555',
+      accuracy: 000,
+      address: 'POST TEST ADDRESS',
+      delivery: 000,
+      name: 'POST TEST NAME',
+      number: 'POST TEST NUMBER 555-555',
       picture: 'http://TEST.TEST.TEST',
-      quality: 36,
-      stars: 3,
+      quality: 000,
+      stars: 000,
     });
     let later = Date.now();
     sum += later - now;
@@ -48,3 +48,59 @@ const getPostAverage = async (dbName, port) => {
 // // Log POST averages for both dbs and log the results
 // getPostAverage('PostgreSQL', 3001);
 // getPostAverage('Cassandra', 4000);
+
+
+// Logs a Put (update) average time.
+const getPutAverage = async (dbName, port) => {
+  let sum = 0;
+  // So that the random restaurant ids can be logged later on.
+  let rands = [];
+
+  for (let i = 0; i < iterations; i += 1) {
+    let rand = get1To10M();
+    rands.push(rand);
+
+    let now = Date.now();
+    await axios.put(`http://localhost:${port}/restaurants/${rand}`, {
+      address: 'PUT TEST ADDRESS',
+      number: 'PUT TEST NUMBER 555-555',
+      quality: 000,
+      stars: 000,
+    });
+
+    let later = Date.now();
+    sum += later - now;
+  }
+
+  console.log('Random restaurants changed: ', rands);
+  console.log(`${dbName} ${iterations} PUTs avg: `, sum / iterations, 'ms');
+};
+
+// // Get PUT averages for both dbs and log the results
+// getPutAverage('PostgreSQL', 3001);
+// getPutAverage('Cassandra', 4000);
+
+
+// Logs a Delete (update) average time.
+const getDeleteAverage = async (dbName, port) => {
+  let sum = 0;
+  // So that the random restaurant ids can be logged later on.
+  let rands = [];
+
+  for (let i = 0; i < iterations; i += 1) {
+    let rand = get1To10M();
+    rands.push(rand);
+
+    let now = Date.now();
+    await axios.delete(`http://localhost:${port}/restaurants/${rand}`);
+    let later = Date.now();
+    sum += later - now;
+  }
+
+  console.log('Random restaurants deleted: ', rands);
+  console.log(`${dbName} ${iterations} DELETEs avg: `, sum / iterations, 'ms');
+};
+
+// // Get Delete averages for both dbs and log the results
+// getDeleteAverage('PostgreSQL', 3001);
+// getDeleteAverage('Cassandra', 4000);
